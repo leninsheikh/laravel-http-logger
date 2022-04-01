bcd4d37439e9725e77b324b1ec4c208b1dd3b389
Lenin Sheikh <justtlenin@gmail.com>
2022-04-01 23:49:42 +0600

    added http logger macro and fix provider path in composer.json

--- src/Providers/LaravelHttpLoggerServiceProvider.php
+++ src/Providers/LaravelHttpLoggerServiceProvider.php
@@ -3,7 +3,9 @@ namespace Leninsheikh\LaravelHttpLogger\Providers;
 
 use Illuminate\Http\Client\Events\RequestSending;
 use Illuminate\Http\Client\Events\ResponseReceived;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Leninsheikh\LaravelHttpLogger\Listeners\LogRequestReceiving;
 use Leninsheikh\LaravelHttpLogger\Listeners\LogRequestSending;
 
@@ -31,5 +33,20 @@ class LaravelHttpLoggerServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->bootHttpMacro();
+    }
+
+    private function bootHttpMacro()
+    {
+
+        Http::macro('withLogging', function ($name) {
+            $key = Str::uuid()->toString();
+
+            return Http::withOptions(['query' => [
+                'lnn_http_logger_key' => $key,
+                'lnn_http_logger_name' => $name,
+            ]]);
+        });
     }
 }
